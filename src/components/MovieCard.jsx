55948275fa59aa3484e7d4f70c249c67cd790393
manika <manikamanipathak@gmail.com>
2025-05-28 18:57:53 +0530

    changes in movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,11 +3,11 @@ import react from 'react';
 const MovieCard = ({movie : {title, vote_average, poster_path, release_date, original_language }}) => {
     return (
         <div className="movie-card">
-           <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : "https://res.cloudinary.com/dhge5bwvy/image/upload/v1747663082/no-movie_ujhqzi.png"} alt="" onError="this.src = 'https://res.cloudinary.com/dhge5bwvy/image/upload/v1747663082/no-movie_ujhqzi.png'"/>
+           <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : "https://res.cloudinary.com/dhge5bwvy/image/upload/v1747663082/no-movie_ujhqzi.png"} alt="Movie"/>
            <h3>{title}</h3>
            <div className="content">
                 <div className="rating">
-                    <img src="/public/star.svg" alt=""/>
+                    <img src="https://res.cloudinary.com/dhge5bwvy/image/upload/v1748438588/star_gpcedu.svg" alt=""/>
                     <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                     <p className='ml-2 font-dm-sans font-light'>• {original_language} • {release_date ? release_date.split('-')[0] : 'N/A'}</p>
                 </div>      
